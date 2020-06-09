389fd847ffaec7d3fc5508307ee50b54c2ffa0ea
qianzai <2794127547@qq.com>
2020-06-09 18:14:54 +0800

    updata

---
+++ 源码/mysql01/src/test/java/ink/bzm/mysql01/Mysql01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.bzm.mysql01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mysql01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
